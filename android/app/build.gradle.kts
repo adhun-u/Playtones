aaa4ef50517562010144909b5ab7e289805e019f
Midhun U <midhunvkm689@gmail.com>
2025-07-02 17:03:10 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,4 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
+
 
 android {
     namespace = "com.example.playtones"
